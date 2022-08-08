a4e57e459695b2752f6a9f5eb8350c603984c0a2
Hänggi Rouven <hr7@suva.ch>
2022-08-08 13:29:41 +0200

    :)

--- shorty.ts
+++ shorty.ts
@@ -15,7 +15,6 @@ const client = new MongoClient({
 });
 
 interface ShortsSchema {
-  _id: ObjectId;
   url: string;
   short: string;
 }
@@ -23,17 +22,17 @@ interface ShortsSchema {
 const db = client.database("shorty");
 const shorts = db.collection<ShortsSchema>("shorts");
 
-export async function newShort(url: string): Promise<string> {
+export async function newShort(url: string): Promise<{url:string, short:string}> {
   const existingUrl = await shorts.findOne({ url })
   if (existingUrl) {
-    return existingUrl.short;
+    return { url, short: existingUrl.short };
   }
   const short = naturalToRoman(await shorts.countDocuments() + 1);
   await shorts.insertOne({
     url,
     short,
-  } as ShortsSchema);
-  return short;
+  });
+  return { url, short };
 }
 
 export async function getUrl(short: string): Promise<{url:string, short:string}> {
