269dc5a9a4b94b728bd38421dd119b4a0d051ac1
Hänggi Rouven <hr7@suva.ch>
2022-08-08 13:55:34 +0200

    :)

--- shorty.ts
+++ shorty.ts
@@ -3,14 +3,13 @@ import {
   ObjectId,
 } from "https://deno.land/x/atlas_sdk@v1.0.2/mod.ts";
 import { naturalToRoman } from "https://deno.land/x/roman_number_utils@1.0.1/mod.ts";
-import { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
-const { DATA_API_KEY, APP_ID } = config();
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 
 const client = new MongoClient({
-  endpoint: "https://data.mongodb-api.com/app/" + APP_ID + "/endpoint/data/v1",
+  endpoint: "https://data.mongodb-api.com/app/" + Deno.env.get("APP_ID") || "" + "/endpoint/data/v1",
   dataSource: "iccee0",
   auth: {
-    apiKey: DATA_API_KEY,
+    apiKey: Deno.env.get("DATA_API_KEY") || "",
   },
 });
 
